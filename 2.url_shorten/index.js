ee4b42d1450c3c2667f66fa72b821ba1bf733c87
DianaFarhat <dsf04@mail.aub.edu>
2025-01-15 13:10:35 +0200

    Add other functunalities

--- 2.url_shorten/index.js
+++ 2.url_shorten/index.js
@@ -1,23 +1,38 @@
-
+// Get Required Libraries
 const express= require ("express");
 const app= express();
 
 const mongoose= require("mongoose");
+const ShortUrl= require('./models/shortUrl');
 
+
+//Establish DB Connection
 mongoose.connect('mongodb+srv://dianasfarhat:Cr3tRBkk0TI3nIhM@cluster0.lsggv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
  
 }).then(() => console.log('Connected to MongoDB Atlas!'))
 .catch((error) => console.error('Error connecting to MongoDB Atlas:', error));
 
-
+//Routes
+app.use(express.urlencoded({extended:false}))
 app.set('view engine', 'ejs');
-app.get('/', (req, res) => {
-    res.render('index');
+app.get('/', async (req, res) => {
+    const shortUrls= await ShortUrl.find();
+    res.render('index', {shortUrls, shortUrls});
   });
   
-app.post('/shortUrls', (req, res)=>{
-
+app.post('/shortUrls', async (req, res)=>{
+  await ShortUrl.create({full: req.body.fullUrl});
+  res.redirect('/');
 })
 
+app.get('/:shortUrl', async (req, res)=> {
+  const shortUrl= await ShortUrl.findOne({short: req.params.shortUrl})
+  if (shortUrl== null){
+    return res.sendStatus(404);
+  }
+  shortUrl.clicks++;
+  shortUrl.save();
+  res.redirect(shortUrl.full);
+})
 
 app.listen(process.env.PORT || 3000);
