9c794809cfb36205ad0fdd1ab8283e61570e8696
DianaFarhat <dsf04@mail.aub.edu>
2025-01-21 12:14:28 +0200

    Add mp3

--- 3.pdf-merge/routes/routes.js
+++ 3.pdf-merge/routes/routes.js
@@ -38,32 +38,6 @@ const mergepdffilter= function(req, file, callback){
 var mergepdffilesupload= multer({storage: storage, fileFilter: mergepdffilter})
 
 
-/*
-router.post('/mergepdfs', mergepdffilesupload.array('files', 100), (req, res) => {
-    const fileList=[];
-    outputFilePath= Date.now()+ "merged.pdf"
-    if(req.files){
-        req.files.forEach(file=> {
-            console.log(file.path)
-            fileList.push(file.path)
-        })
-        console.log(fileList)
-        pdfMerge(fileList, outputFilePath, function(err) {
-            if(err){
-                res.send(err)
-            }
-            res.download(outputFilePath, (err)=> {
-                if(err){
-                    res.send(err)
-                }
-                fs.unlinkSync(outputFilePath)
-            })
-            
-        })
-    }
-});
-*/
-
 router.post('/mergepdfs', mergepdffilesupload.array('files', 100), (req, res) => {
     // Ensure files are provided
     if (!req.files || req.files.length === 0) {
